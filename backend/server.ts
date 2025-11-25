d51ea5c843f0d94ed999334882fb6d73c7a906e1
CodeExcalibar <Alex.raju1010@gmail.com>
2025-11-25 10:13:36 +0000

    Removed the console.log styles

--- backend/server.ts
+++ backend/server.ts
@@ -5,14 +5,6 @@ const port = 3000;
 
 app.use(express.json());
 
-
-// ANSI Escape Codes:
-const RESET = '\x1b[0m';
-const CYAN = '\x1b[36m';
-const YELLOW = '\x1b[33m';
-
 app.listen(port, () => {
-  console.log(
-    `${CYAN}Server listenings on http://localhost:${YELLOW}${port}${RESET}`,
-  );
+  console.log(`Server listenings on http://localhost:${port}`);
 });
