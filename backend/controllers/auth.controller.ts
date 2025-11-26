a788eb58ee135b4f19a7b16c6727a7c2c477be38
CodeExcalibar <Alex.raju1010@gmail.com>
2025-11-26 15:11:05 +0000

    Implemented signup token creation

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from 'express';
 import User from '../models/user.model';
-import jwt from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
+
+const signToken = (id: string): string => {
+  const secret: Secret = process.env.JWT_SECRET!;
+  const expiresIn = process.env.JWT_EXPIRES_IN;
+  return jwt.sign({ id }, secret, { expiresIn: expiresIn as any });
+};
 
 export const signUp = async (req: Request, res: Response) => {
   const { username, email, password, confirmPassword } = req.body;
+
   const newUser = await User.create({
     username,
     email,
@@ -11,9 +18,13 @@ export const signUp = async (req: Request, res: Response) => {
     confirmPassword,
   });
 
+  console.log(typeof newUser.id);
+  //   console.log(typeof (process.env.JWT_SECRET as Secret));
+  const token = signToken(String(newUser.id));
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
