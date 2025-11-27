04141c2a9be5f7b0f6949de66650aab899027f1d
CodeExcalibar <Alex.raju1010@gmail.com>
2025-11-27 15:38:31 +0000

    Implemented update workspace endpoint

--- backend/controllers/workspace.controller.ts
+++ backend/controllers/workspace.controller.ts
@@ -1,8 +1,11 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Workspace from '../models/workspace.model';
+import AppError from '../lib/AppError';
 
 export const getAllWorkspace = async (_: Request, res: Response) => {
-  const workspaces = await Workspace.findAll();
+  const workspaces = await Workspace.findAll({
+    order: [['id', 'asc']],
+  });
 
   res.status(200).json({ status: 'success', results: workspaces.length, data: { workspaces } });
 };
@@ -14,4 +17,21 @@ export const createWorkspace = async (req: Request, res: Response) => {
   const workspace = await Workspace.create({ name });
 
   res.status(200).json({ status: 'success', data: { workspace } });
+};
+
+export const updateWorkspace = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const workspace = await Workspace.findByPk(id);
+
+  if (!workspace) {
+    return next(new AppError('Workspace with this ID does not exist', 404));
+  }
+
+  const updatedWorkspace = await workspace.update({ name });
+
+  res.status(200).json({
+    status: 'success',
+    data: updatedWorkspace,
+  });
 };
--- backend/routes/workspace.route.ts
+++ backend/routes/workspace.route.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import { createWorkspace, getAllWorkspace } from '../controllers/workspace.controller';
+import {
+  createWorkspace,
+  getAllWorkspace,
+  updateWorkspace,
+} from '../controllers/workspace.controller';
 import { protect } from '../controllers/auth.controller';
 
 export const workspaceRouter = Router();
 
 workspaceRouter.route('/').get(protect, getAllWorkspace).post(protect, createWorkspace);
+
+workspaceRouter.route('/:id').patch(updateWorkspace);
