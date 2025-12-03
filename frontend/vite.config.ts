b8d9bf3101d46af67cc61ed2c33be74dfeeb0cc3
CodeExcalibar <Alex.raju1010@gmail.com>
2025-12-03 21:43:49 +0000

    turning of typescript

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import viteReact from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import viteReact from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    devtools(),
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    viteReact(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-})
+	plugins: [
+		devtools(),
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		viteReact({
+			tsCheck: false, // <-- disables type checking
+		}),
+		viteReact(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
